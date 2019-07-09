c12602b67656e9794ffd6106078d0b36ad1719e4
moacirviana <moacir.viana@gmail.com>
2019-07-09 11:18:34 -0400

    ajustes e melhorias

--- WebContent/forms/dataeleicao.js
+++ WebContent/forms/dataeleicao.js
@@ -1,23 +1,23 @@
 /* DATA ELEICAO  */
 $(document).ready(function() {
-	
- $("#btnSave").click(function() {
- var URL = ""; 
-	 if ( $('#cd').length ) { URL = "atualizar"; }
-	 else{ URL = "adicionar";  }	 
-	 swal({
-         title: "Confirma ?",
-         text: "Confirma " + URL + "?",
-         icon: "info",
-         buttons: true,
-         dangerMode: true,
-         })
-         .then((resp) => {
+	 // CLICK DO BOTÃO SAVE 
+	 $("#btnSave").click(function() {
+		var URL = ""; 
+		if ( $('#id').length ) { URL = "atualizar"; }
+		else{ URL = "adicionar";  }	 
+		 swal({
+	         title: "Confirma ?",
+	         text: "Confirma " + URL + "?",
+	         icon: "info",
+	         buttons: true,
+	         dangerMode: true,
+	         })
+	         .then((resp) => {
 				if (resp) {
 					var frm = $("#form1").serialize();					
 					$.getJSON({
-							url: URL,
-							data: frm
+						url: URL,
+						data: frm
 				    }).done(function( data ) {
 				    	if(data.ret==1)
 							 swal(URL, data.mensagem, "success");
@@ -26,10 +26,7 @@ $(document).ready(function() {
 					}).fail(function() {
 								swal("Update", "An error occurred", "error");
 					});
-
-			    } 
-			});
-	 
-  });
-   
+			      } 
+		   }); // -- FIM SWAL --
+	 	}); 
 });
