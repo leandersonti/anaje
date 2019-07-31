5f4e3b2479bc381c0ca019fd25dd6039f4032309
moacirviana <moacir.viana@gmail.com>
2019-07-31 10:58:08 -0400

    Classe permissão

---
+++ src/br/jus/tream/action/Permissao.java
@@ -0,0 +1,35 @@
+package br.jus.tream.action;
+
+import java.io.Serializable;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
+
+import br.jus.tream.dominio.BeanLogin;
+
+@SuppressWarnings("serial")
+public class Permissao implements Serializable{
+	static Permissao db;
+
+	public static Permissao getInstance() {
+		if (db == null) {
+			db = new Permissao();
+		}
+		return db;
+	}
+	
+	public final boolean getAdmin() {
+		HttpSession session = ServletActionContext.getRequest().getSession(true);
+    	BeanLogin b = (BeanLogin)session.getAttribute("login");
+	    return (b.getAdmin()==1);
+	}
+	
+	public final int getZona() {
+		HttpSession session = ServletActionContext.getRequest().getSession(true);
+    	BeanLogin b = (BeanLogin)session.getAttribute("login");
+	    return b.getZona();
+	}
+	
+	
+}
