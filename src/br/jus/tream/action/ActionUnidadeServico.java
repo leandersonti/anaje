f7943c31015e51d2d45bef964028e32fec5ef2bd
Expedito Almeida <expedito_dealmeida@outlook.com.br>
2019-08-05 11:34:55 -0400

    Ajuste para listar zona

--- src/br/jus/tream/action/ActionUnidadeServico.java
+++ src/br/jus/tream/action/ActionUnidadeServico.java
@@ -11,6 +11,7 @@ import org.apache.struts2.convention.annotation.ResultPath;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import br.jus.tream.DAO.CadEloDAOImpl;
 import br.jus.tream.DAO.UnidadeServicoDAO;
 import br.jus.tream.DAO.UnidadeServicoDAOImpl;
 import br.jus.tream.dominio.BeanResult;
@@ -27,6 +28,7 @@ public class ActionUnidadeServico extends ActionSupport {
 	private List<UnidadeServico> lstUnidadeServico;
 	private List<CADLocalvotacao> lstLocalVotacao;
 	private List<CADZonaEleitoral> lstZonaEleitoral;
+	private CADZonaEleitoral cadZonaEleitoral;
 	private UnidadeServico uservico;
 	private IDEleicaoPK id = new IDEleicaoPK();
 	private BeanResult result;
@@ -60,7 +62,12 @@ public class ActionUnidadeServico extends ActionSupport {
 			@Result(name = "error", location = "/pages/error.jsp") }, interceptorRefs = @InterceptorRef("authStack"))
 	public String frmCadUnidadeServico() {
 		try {
-			this.lstUnidadeServico = UnidadeServicoDAOImpl.getInstance().listar();
+
+			if (permissao.getAdmin()) {
+				this.lstZonaEleitoral = CadEloDAOImpl.getInstance().listarZonaEleitoral();
+			} else {
+				this.lstZonaEleitoral = CadEloDAOImpl.getInstance().listarZonaEleitoral(permissao.getZona());
+			}
 
 		} catch (Exception e) {
 			addActionError(getText("frmsetup.error") + " Error: " + e.getMessage());
@@ -189,5 +196,15 @@ public class ActionUnidadeServico extends ActionSupport {
 	public void setResult(BeanResult result) {
 		this.result = result;
 	}
+
+	public CADZonaEleitoral getCadZonaEleitoral() {
+		return cadZonaEleitoral;
+	}
+
+	public void setCadZonaEleitoral(CADZonaEleitoral cadZonaEleitoral) {
+		this.cadZonaEleitoral = cadZonaEleitoral;
+	}
+	
+	
 
 }
