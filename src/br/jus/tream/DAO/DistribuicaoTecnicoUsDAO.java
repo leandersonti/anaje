d13e176cc21bf110767614933191f2b74fd68def
Moacir Viana <moacir.viana@tre-am.jus.br>
2019-09-19 11:26:15 -0400

    ALTERAÇÃO DOS METODOS

--- src/br/jus/tream/DAO/DistribuicaoTecnicoUsDAO.java
+++ src/br/jus/tream/DAO/DistribuicaoTecnicoUsDAO.java
@@ -2,22 +2,26 @@ package br.jus.tream.DAO;
 
 import java.util.List;
 
-import br.jus.tream.dominio.CADSecao;
 import br.jus.tream.dominio.DistribuicaoTecnicoUS;
+import br.jus.tream.dominio.Tecnico;
+import br.jus.tream.dominio.pk.CadZonaEleitoralPK;
 
 public interface DistribuicaoTecnicoUsDAO {
 	
-	public List<DistribuicaoTecnicoUS> listar(Integer idUnidadeServico) throws Exception;
+	public List<DistribuicaoTecnicoUS> listar(CadZonaEleitoralPK codzonaMunic, Integer contrato) throws Exception;
 		
-	public List<CADSecao> listarParaDistribuir(Integer zona, Integer codmunic, Integer numlocal) throws Exception;
+	public List<DistribuicaoTecnicoUS> listar(CadZonaEleitoralPK codzonaMunic) throws Exception;
 	
-	public List<DistribuicaoTecnicoUS> listar(Integer zona, Integer codmunic) throws Exception;	 
-
-	public DistribuicaoTecnicoUS getBean(String codibjeto) throws Exception;
+	public List<Tecnico> listarParaDistribuir() throws Exception;
+	
+	public List<Tecnico> listarParaDistribuir(Integer zona) throws Exception;
+	
+	// CONSULTA PELO NUMERO DO TITULO DO TECNICO
+	public DistribuicaoTecnicoUS getBean(String tituloEleitor) throws Exception;
 	
 	public int adicionar (DistribuicaoTecnicoUS ds) throws Exception;
 	
-	public int atualizar (DistribuicaoTecnicoUS ds) throws Exception;
+	// public int atualizar (DistribuicaoTecnicoUS ds) throws Exception;
 	
 	public int remover (DistribuicaoTecnicoUS ds) throws Exception;
 	
