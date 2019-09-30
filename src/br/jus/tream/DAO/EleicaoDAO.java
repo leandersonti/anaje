5e66be2f0554c10eaede89211f235a17d6f92702
Moacir Viana <moacir.viana@tre-am.jus.br>
2019-09-30 12:03:39 -0400

    ajustes e correções

--- src/br/jus/tream/DAO/EleicaoDAO.java
+++ src/br/jus/tream/DAO/EleicaoDAO.java
@@ -2,23 +2,23 @@ package br.jus.tream.DAO;
 
 import java.util.List;
 
-import br.jus.tream.dominio.DataEleicao;
+import br.jus.tream.dominio.Eleicao;
 
-public interface DataEleicaoDAO {
+public interface EleicaoDAO {
 	
-	public List<DataEleicao> listar() throws Exception;
+	public List<Eleicao> listar() throws Exception;
 	
-	public List<DataEleicao> listarCbx() throws Exception; // para preenchimento de combobox
+	public List<Eleicao> listarCbx() throws Exception; // para preenchimento de combobox
 	
-	public DataEleicao getBeanAtiva( ) throws Exception;
+	public Eleicao getBeanAtiva( ) throws Exception;
 	
-	public DataEleicao getBean(Integer id) throws Exception;
+	public Eleicao getBean(Integer id) throws Exception;
 	
-	public int adicionar (DataEleicao dateEleicao) throws Exception;
+	public int adicionar (Eleicao eleicao) throws Exception;
 	
-	public int atualizar (DataEleicao dateEleicao) throws Exception;
+	public int atualizar (Eleicao eleicao) throws Exception;
 	
-	public int remover (DataEleicao dateEleicao) throws Exception;
+	public int remover (Eleicao eleicao) throws Exception;
 	
 	public int ativar (int id) throws Exception;
 	
