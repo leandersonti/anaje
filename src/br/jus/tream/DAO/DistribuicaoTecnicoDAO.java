f4483e65da4d12a7dd4ed9e591b77c8f2d1a8fb0
Moacir Viana <moacir.viana@tre-am.jus.br>
2019-09-30 14:18:19 -0400

    ajustes e melhorias

--- src/br/jus/tream/DAO/DistribuicaoTecnicoDAO.java
+++ src/br/jus/tream/DAO/DistribuicaoTecnicoDAO.java
@@ -2,31 +2,31 @@ package br.jus.tream.DAO;
 
 import java.util.List;
 
-import br.jus.tream.dominio.DistribuicaoTecnicoUS;
+import br.jus.tream.dominio.DistribuicaoTecnico;
 import br.jus.tream.dominio.Tecnico;
 import br.jus.tream.dominio.pk.CadZonaEleitoralPK;
 
-public interface DistribuicaoTecnicoUsDAO {
+public interface DistribuicaoTecnicoDAO {
 	
-	public List<DistribuicaoTecnicoUS> listar(CadZonaEleitoralPK codzonaMunic, Integer contrato) throws Exception;
+	public List<DistribuicaoTecnico> listar(CadZonaEleitoralPK codzonaMunic, Integer contrato) throws Exception;
 		
-	public List<DistribuicaoTecnicoUS> listar(CadZonaEleitoralPK codzonaMunic) throws Exception;
+	public List<DistribuicaoTecnico> listar(CadZonaEleitoralPK codzonaMunic) throws Exception;
 	
 	public List<Tecnico> listarParaDistribuir() throws Exception;
 	
 	public List<Tecnico> listarParaDistribuir(Integer zona) throws Exception;
 	
 	// CONSULTA PELO NUMERO DO TITULO DO TECNICO
-	public DistribuicaoTecnicoUS getBean(String tituloEleitor) throws Exception;
+	public DistribuicaoTecnico getBean(String tituloEleitor) throws Exception;
 	
 	// CONSULTA PELA UNIDADE SERVICO
-	public DistribuicaoTecnicoUS getBean(Integer unidadeservico) throws Exception;
+	public DistribuicaoTecnico getBean(Integer unidadeservico) throws Exception;
 	
-	public int adicionar (DistribuicaoTecnicoUS ds) throws Exception;
+	public int adicionar (DistribuicaoTecnico ds) throws Exception;
 	
 	// public int atualizar (DistribuicaoTecnicoUS ds) throws Exception;
 	
-	public int remover (DistribuicaoTecnicoUS ds) throws Exception;
+	public int remover (DistribuicaoTecnico ds) throws Exception;
 	
 	
 } 
